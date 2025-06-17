9bce4924b5dd8567159470de8ed0ef5053f7ef9c
Pawan Prasad Sahu <sahupawanprasad@gmail.com>
2025-06-17 05:37:29 +0000

    config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 14455345
+let accountEmail = "pawansahu@google.com"
+var accountPassword = "12345"
+accountCity = "jashpur"
+
+// accountId = 2
+// console.log(accountId)
+accountEmail = "pawansahu@microsoft.com"
+// console.log(accountEmail)
+accountPassword = "000000"
+// console.log(accountPassword)
+accountCity = "ranchi"
+// console.log(accountCity)
+
+console.table([accountEmail, accountId, accountPassword, accountCity])
+
+/*please do not use var ever
+becouse of issue in block scope and functional scope*/
