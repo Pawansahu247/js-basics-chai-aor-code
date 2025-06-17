cb829f40b4af1889aef823aa47ba2bdd2be29e73
Pawan Prasad Sahu <sahupawanprasad@gmail.com>
2025-06-17 08:35:28 +0000

    config

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+// alert(3+3) we are using node js, not browser
+
+
+let name = "pawwan"
+let age = 18
+let isLoggedIn = false
+
+/*number 2to 53
+bigint
+string = ""
+boolean true/false
+null = standalone value
+undefined = 
+symbol = 
+*/
+
+// object
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
